b74e8b12a14c61105355b299ecbabb000c012b4c
FabricioCaixeta <106625098+FabricioCaixeta@users.noreply.github.com>
2024-09-09 02:13:39 -0300

    Teste 1

--- teste1.py
+++ teste1.py
@@ -1,11 +1,11 @@
-def sequencia_fibonacci(limit):
+def sequencia_fibonacci(limit):  #Esta Função gera a sequencia de fibonacci
     sequence = [0, 1]
-    while sequence[-1] < limit:
+    while sequence[-1] < limit:    
         next_value = sequence[-1] + sequence[-2]
         sequence.append(next_value)
     return sequence
 
-def esta_na_sequencia(number):
+def esta_na_sequencia(number):   #Esta função verifica se o número informado está na sequência
     sequence = sequencia_fibonacci(number)
     if number in sequence:
         return f"O número {number} pertence à sequência de Fibonacci."
