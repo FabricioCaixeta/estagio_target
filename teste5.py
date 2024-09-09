33d582ae44743ff314156942e70e603c792f293f
FabricioCaixeta <106625098+FabricioCaixeta@users.noreply.github.com>
2024-09-09 02:28:32 -0300

    Resolução teste 5

---
+++ teste5.py
@@ -0,0 +1,13 @@
+# Procedimento:
+
+# Ligue o primeiro interruptor e deixe-o ligado por um tempo (por exemplo, 10 minutos). Esse tempo é importante para que a lâmpada conectada a esse interruptor tenha tempo suficiente para esquentar.
+
+# Depois de 10 minutos, desligue o primeiro interruptor e ligue o segundo interruptor.
+
+# Agora, vá até a sala das lâmpadas.
+
+# Identifique as lâmpadas e compare com as seguintes características:
+
+# A lâmpada que está acesa é controlada pelo segundo interruptor (o que você acabou de ligar).
+# A lâmpada que está apagada mas ainda quente é controlada pelo primeiro interruptor (o que você deixou ligado por um tempo e depois desligou).
+# A lâmpada que está apagada e fria é controlada pelo terceiro interruptor (o que você não ligou).
